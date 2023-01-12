cf10b26f913791fb89601d8ff2547a1838e70417
CharlesBCookman <CharlesBCookman@gmail.com>
2023-01-11 16:33:17 -0800

    Saving progress.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function getValuesAndSet() {
+    name1value = document.querySelector("form#name1").value
+    name2value = document.querySelector("form#name2").value
+    placevalue = document.querySelector("form#place").value
+    noun1value = document.querySelector("form#noun1").value
+}
